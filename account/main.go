8c8f6eba87f6b08537f6453a3595a21e039fcabd
Rahmat Hanafi <59599805+j03hanafi@users.noreply.github.com>
2023-09-19 21:34:14 +0700

    Setup Go server (#1)
    * setup go server with reload in docker
    
    * Update docker image
    
    ---------
    
    Co-authored-by: j03hanafi <rahmat.hanafi.konsultan@work.bri.co.id>

---
+++ account/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	// Setup zap logger
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		log.Fatalf("Failed to initialize logger: %v", err)
+	}
+	defer logger.Sync()
+
+	logger.Info("Server is starting...")
+
+	router := gin.Default()
+
+	router.GET("/api/account", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"hello": "public!",
+		})
+	})
+
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":8008",
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			logger.Fatal("Failed to initialize server: ",
+				zap.Error(err),
+			)
+		}
+	}()
+
+	logger.Info("Starting server",
+		zap.String("address", server.Addr),
+	)
+
+	// Kill signal channel to shut down
+	quit := make(chan os.Signal)
+
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	// block until kill signal received
+	<-quit
+
+	// Context for informing server to close the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Shut down server
+	logger.Info("Server is shutting down...")
+	if err := server.Shutdown(ctx); err != nil {
+		logger.Fatal("Server forced to shut down",
+			zap.Error(err),
+		)
+	}
+}
