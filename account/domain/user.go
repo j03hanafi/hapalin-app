aff90f599567687c680b43c815eaf8714e4dc3a7
j03hanafi <namoapak14@gmail.com>
2023-09-22 17:22:24 +0700

    Setup clean architecture

---
+++ account/domain/user.go
@@ -0,0 +1,28 @@
+package domain
+
+import (
+	"context"
+	"github.com/google/uuid"
+)
+
+// User defines domain model and its json and db representations
+type User struct {
+	UID      uuid.UUID `json:"uid" db:"uid"`
+	Email    string    `json:"email" db:"email"`
+	Password string    `json:"-" db:"password"`
+	Name     string    `json:"name" db:"name"`
+	ImageURL string    `json:"image_url" db:"image_url"`
+	Website  string    `json:"website" db:"website"`
+}
+
+// UserService defines methods the handler layer expects
+// any service it interacts with to implement
+type UserService interface {
+	Get(ctx context.Context, uid uuid.UUID) (*User, error)
+}
+
+// UserRepository defines methods the service layer expects
+// any repository it interacts with to implement
+type UserRepository interface {
+	FindByID(ctx context.Context, uid uuid.UUID) (*User, error)
+}
